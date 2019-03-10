2753138885b9b427ffbeec54a0ea351a6bb1504c
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-03-09 16:47:11 -0800

    works with function

--- playground.py
+++ playground.py
@@ -1,69 +1,25 @@
 ***REMOVED***
 ***REMOVED***
-# # Your Account Sid and Auth Token from twilio.com/console
-# account_sid = 'AC6f773e33d173baceaddd67ddc7f3d124'
-# auth_token = '67d7171a58c916ab3531e8a572aa24cc'
-# ***REMOVED***
-
-# call = client.calls.create(
-# 	method='GET',
-# 	status_callback='https://www.myapp.com/events',
-# 	status_callback_event=['initiated', 'answered'],
-# 	status_callback_method='POST',
-# 	url='http://demo.twilio.com/docs/voice.xml',
-# 	to='+16505461126',
-# 	from_='+16508806260'
-# )
-
-
-
-# print(call.sid)
-# print("done")
-
-# Download the helper library from https://www.twilio.com/docs/python/install
 ***REMOVED***
-from pprint import pprint
-# ***REMOVED***
 
 
-# # Your Account Sid and Auth Token from twilio.com/console
 account_sid = 'AC6f773e33d173baceaddd67ddc7f3d124'
 auth_token = '67d7171a58c916ab3531e8a572aa24cc'
 ***REMOVED***
-call = client.calls.create(
-                ***REMOVED***
-                        # status_callback='https://fordaniel.pythonanywhere.com/',
-                        # status_callback_event=['initiated', 'completed', 'busy'],
-                        # status_callback_method='POST',
-                ***REMOVED***
-                        to ='+16505461126',
-                        from_='+16508806260'
-                ***REMOVED***
-time.sleep(3)
-call_sid = call.sid 
-# # client.calls(call.sid).delete()
-# call = client.calls(call.sid) 
 
-# print(call.to)
-# ***REMOVED***
+***REMOVED***
+	call = client.calls.create(
+		method='GET',
+		url='http://demo.twilio.com/docs/voice.xml',
+		to =phone_number,
+		from_='+16508806260'
+	)
+	time.sleep(3)
+	call_sid = call.sid 
+	calls = client.calls.list(status="busy")
+	for busy_call in calls:
+		if busy_call.sid == call_sid:
+		 	return "busy"
+	return "not busy"
 
-
-# # Your Account Sid and Auth Token from twilio.com/console
-# account_sid = 'AC6f773e33d173baceaddd67ddc7f3d124'
-# auth_token = '67d7171a58c916ab3531e8a572aa24cc'
-# ***REMOVED***
-
-calls = client.calls.list(
-	status="busy"
-)
-# print()
-# print(calls[-1].status)
-for busy_call in calls:
-	if busy_call.sid == call_sid:
-		print("busy")
-# print(len(calls), calls[0].status, "calls")
-
-# recent = calls[-1]
-# print(recent.status)
-
-	
+print(check_number_busy("+16505461126"))
