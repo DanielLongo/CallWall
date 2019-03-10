4daf94d421591081d2e3f2bacf6e9862d95883ee
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-03-09 18:55:09 -0800

    first

---
+++ twilio_busy.py
@@ -0,0 +1,25 @@
+import twilio
+from twilio.rest import Client
+import time
+
+
+account_sid = 'AC6f773e33d173baceaddd67ddc7f3d124'
+auth_token = 'x'
+client = Client(account_sid, auth_token)
+
+def check_number_busy(phone_number):
+	call = client.calls.create(
+		method='GET',
+		url='http://demo.twilio.com/docs/voice.xml',
+		to =phone_number,
+		from_='+16508806260'
+	)
+	time.sleep(3)
+	call_sid = call.sid 
+	calls = client.calls.list(status="busy")
+	for busy_call in calls:
+		if busy_call.sid == call_sid:
+		 	return "busy"
+	return "not busy"
+
+# print(check_number_busy("+16505461126"))
